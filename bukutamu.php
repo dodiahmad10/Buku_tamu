a40f149ff36a2071751c1676df3436cc505e843f
dodiahmad10 <82955279+dodiahmad10@users.noreply.github.com>
2021-05-07 21:05:59 +0700

    Buku_tamu

---
+++ bukutamu.php
@@ -0,0 +1,16 @@
+
+<html>
+<head>
+	<title>Buku Tamu</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div align="center">
+		<?php require("content_bukutamu.php"); ?>
+		<br>
+		<?php require("report_bukutamu.php"); ?>
+	</div>
+
+</body>
+</html>
